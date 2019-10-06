a3c62a179c1c967a745df0ea28abe65e8d5b1610
sarah <s.j.condon01@gmail.com>
2019-10-06 23:35:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,8 +36,8 @@ class CashRegister
   end
 
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
+   def void_last_transaction
+    self.total - self.last_transaction
   end
 
 end
